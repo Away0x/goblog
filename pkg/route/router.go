fe031636cdb5010b1ef3cb23e2fcc6ae6bb4e58d
Away0x <wutong0910@foxmail.com>
2020-11-22 17:00:27 +0800

    重构更新文章

--- pkg/route/router.go
+++ pkg/route/router.go
@@ -1,6 +1,8 @@
 package route
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -20,4 +22,9 @@ func Name2URL(routeName string, pairs ...string) string {
 	}
 
 	return url.String()
+}
+
+func GetRouteVariable(parameterName string, r *http.Request) string {
+	vars := mux.Vars(r)
+	return vars[parameterName]
 }

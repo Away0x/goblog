5d8686a11cf4235f5390b6233e3dbaf07e728a0a
Away0x <wutong0910@foxmail.com>
2020-11-22 19:39:28 +0800

    会话和认证管理

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -1,6 +1,9 @@
 package user
 
-import "goblog/app/models"
+import (
+	"goblog/app/models"
+	"goblog/pkg/route"
+)
 
 // User 用户模型
 type User struct {
@@ -12,4 +15,15 @@ type User struct {
 
 	// gorm:"-" —— 设置 GORM 在读写时略过此字段，仅用于表单验证
 	PasswordComfirm string `gorm:"-" valid:"password_comfirm"`
+}
+
+// ComparePassword 对比密码是否匹配
+func (u User) ComparePassword(_password string) bool {
+	// return password.CheckHash(_password, u.Password)
+	return u.Password == _password
+}
+
+// Link 方法用来生成用户链接
+func (u User) Link() string {
+	return route.Name2URL("users.show", "id", u.GetStringID())
 }
